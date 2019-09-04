0f4a0a9082ed4008053093527ad67c8b7096e046
schaslivchik <schaslivchikm@gmail.com>
2019-09-04 17:52:51 +0300

    created dynamic popup

--- src/js/index.js
+++ src/js/index.js
@@ -11,9 +11,20 @@ const laneScroll = document.querySelectorAll('.lane__item');
     })
 );
 
-let listGroup = document.querySelectorAll('.list-group');
 
-// listGroup[1].addEventListener('click', () => {
-//     let div = document.createElement('div')
-//     document.querySelector('body').appendChild(div).classList.add('popup')
-// })
+/**
+ * Popup open and close
+ */
+let listGroup = document.querySelectorAll('.list-group');
+let popup = document.querySelector('.popup_block');
+let closeBtn = document.querySelector('.close__popup');
+
+listGroup[1].addEventListener('click', () => {
+    popup.classList.add('open');
+    popup.classList.remove('close');
+})
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.add('close');
+    popup.classList.remove('open');
+})
