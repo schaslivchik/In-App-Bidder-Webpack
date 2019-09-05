72b6abd55d8f0291aa066d5a9b9f27d2847ed4d9
schaslivchik <schaslivchikm@gmail.com>
2019-09-05 17:56:26 +0300

    updated popup

--- src/js/touch.js
+++ src/js/touch.js
@@ -1,51 +1,57 @@
 let swipeZone = document.getElementById('swipezone');
-let dragItem = document.querySelector('.circle-bidding');
 
-// swipedElement.addEventListener('touchstart', ()=> console.log('touch'))
-var active = false;
-    var currentX;
-    var initialX;
-    var xOffset = 0;
 
-    swipeZone.addEventListener("touchstart", dragStart, false);
-    swipeZone.addEventListener("touchend", dragEnd, false);
-    swipeZone.addEventListener("touchmove", drag, false);
+var touchStartCoords =  {'x':-1, 'y':-1}, // X and Y coordinates on mousedown or touchstart events.
+    touchEndCoords = {'x':-1, 'y':-1},// X and Y coordinates on mouseup or touchend events.
+    direction = 'undefined',// Swipe direction
+    minDistanceXAxis = 30,// Min distance on mousemove or touchmove on the X axis
+    maxDistanceYAxis = 30,// Max distance on mousemove or touchmove on the Y axis
+    maxAllowedTime = 4000,// Max allowed time between swipeStart and swipeEnd
+    startTime = 0,// Time on swipeStart
+    elapsedTime = 0,// Elapsed time between swipeStart and swipeEnd
+    targetElement = document.querySelector('.circle-bidding');// Element to delegate
 
-    // swipeZone.addEventListener("mousedown", dragStart, false);
-    // swipeZone.addEventListener("mouseup", dragEnd, false);
-    // swipeZone.addEventListener("mousemove", drag, false);
+function swipeStart(e) {
+  e = e ? e : window.event;
+  e = ('changedTouches' in e)?e.changedTouches[0] : e;
+  touchStartCoords = {'x':e.pageX, 'y':e.pageY};
+  startTime = new Date().getTime();
+  targetElement.textContent = " ";
+}
 
-    function dragStart(e) {
-      if (e.type === "touchstart") {
-        initialX = e.touches[0].clientX - xOffset;
-      } else {
-        initialX = e.clientX - xOffset;
-      }
-      if (e.target === dragItem) {
-        active = true;
-      }
-    }
+function swipeMove(e){
+  e = e ? e : window.event;
+  e.preventDefault();
+}
 
-    function dragEnd(e) {
-      initialX = currentX;
-      active = false;
-    }
-
-    function drag(e) {
-      if (active) {
-        e.preventDefault();
-        if (e.type === "touchmove") {
-          currentX = e.touches[0].clientX - initialX;
-        } else {
-          currentX = e.clientX - initialX;
-        }
-        xOffset = currentX;
-        setTranslate(currentX, dragItem);
+function swipeEnd(e) {
+  e = e ? e : window.event;
+  e = ('changedTouches' in e)?e.changedTouches[0] : e;
+  touchEndCoords = {'x':e.pageX - touchStartCoords.x, 'y':e.pageY - touchStartCoords.y};
+  elapsedTime = new Date().getTime() - startTime;
+  if (elapsedTime <= maxAllowedTime){
+    if (Math.abs(touchEndCoords.x) >= minDistanceXAxis && Math.abs(touchEndCoords.y) <= maxDistanceYAxis){
+      direction = (touchEndCoords.x < 0)? 'left' : 'right';
+      switch(direction){
+        case 'left':
+          alert("Left swipe detected");
+          break;
+        case 'right':
+          alert("Right swipe detected");
+          break;
       }
     }
+  }
+}
 
-    function setTranslate(xPos, el) {
-        el.style.transform = "translateX(" + xPos + "px)";
-     
-    }
-    
+function addMultipleListeners(el, s, fn) {
+  var evts = s.split(' ');
+  for (var i=0, iLen=evts.length; i<iLen; i++) {
+    el.addEventListener(evts[i], fn, false);
+  }
+}
+
+addMultipleListeners(targetElement, 'mousedown touchstart', swipeStart);
+addMultipleListeners(targetElement, 'mousemove touchmove', swipeMove);
+addMultipleListeners(targetElement, 'mouseup touchend', swipeEnd);
+
